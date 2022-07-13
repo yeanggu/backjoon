c4cc1a88bd47c3cb601c34d9ea1175de860a0c4d
yeeun <dpdms9621@naver.com>
2022-07-13 16:17:34 +0900

    브론즈 4문제

---
+++ backJoon/src/bronze5/b1001.java
@@ -0,0 +1,17 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class b1001 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        System.out.println(A-B);
+
+        scan.close();
+	}
+
+}
---
+++ backJoon/src/bronze5/b1008.java
@@ -0,0 +1,17 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class b1008 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        System.out.println((double)A/B);
+
+        scan.close();
+	}
+
+}
