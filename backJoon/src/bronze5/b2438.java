c8b1cb273c4c063c33cc9a3e30c95be8019ad0a9
yeeun <dpdms9621@naver.com>
2022-07-15 16:43:19 +0900

    별찍기 1

---
+++ backJoon/src/bronze5/b2438.java
@@ -0,0 +1,24 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class b2438 {
+	/*
+	 * 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+	 */
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        
+        for(int i=1;i<=A;i++) {
+        	for(int j=1;j<=i;j++) {
+        		System.out.print("*");
+        	}
+        	System.out.println();
+        }
+        
+        scan.close();
+	}
+
+}
