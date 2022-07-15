d3c8e707f93d7d3f3620f44dc4422b41ed97483c
yeeun <dpdms9621@naver.com>
2022-07-15 16:29:32 +0900

    2420번

---
+++ backJoon/src/bronze5/b2420.java
@@ -0,0 +1,29 @@
+package bronze5;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class b2420 {
+	/*
+	 * 사파리월드는 인터넷으로만 존재하는 미스테리한 나라이다. 사파리월드에는 2개의 서브도메인이 seunghwan.royal.gov.sw와
+	 * kyuhyun.royal.gov.sw 이 있는데, 이것이 couple.royal.gov.sw으로 합쳐질 것이다. 그러나 도메인 관리 센터
+	 * SWNIC(센터장: 김동규)에는 엄격한 룰이 있다. 두 서브도메인을 합칠 때, 유명도의 차이가 너무 차이나지 않을 경우에만 두 서브도메인을
+	 * 결혼시키는 것이다. 서브도메인의 유명도는 정수이다. 두 서브도메인의 유명도가 주어졌을 때, 그 차이를 구하는 프로그램을 작성하시오.
+	 */
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        BigInteger A = scan.nextBigInteger();
+        BigInteger B = scan.nextBigInteger();
+        
+        if((A.compareTo(B)>0))
+			System.out.println(A.subtract(B));
+		else
+			System.out.println(B.subtract(A));
+        
+        //BigInteger의 value는 문자열이라서 변수명, 메서드형식으로 연산식 구현
+        
+        scan.close();
+	}
+
+}
