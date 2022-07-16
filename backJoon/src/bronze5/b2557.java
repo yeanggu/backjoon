9d1235acb34e6502ff8cad969c67ec7fc672b04f
yeeun <dpdms9621@naver.com>
2022-07-16 19:38:23 +0900

    3문제

---
+++ backJoon/src/bronze5/b2557.java
@@ -0,0 +1,9 @@
+package bronze5;
+
+public class b2557 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ backJoon/src/bronze5/b2558.java
@@ -0,0 +1,16 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class b2558 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		System.out.println(A+B);
+	}
+
+}
