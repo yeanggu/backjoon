255e81ec0dd9297ab8e9f62ef37d28c72dd8965a
yeeun <dpdms9621@naver.com>
2022-07-18 17:35:41 +0900

    2741문제

--- backJoon/src/bronze5/b2741.java
+++ backJoon/src/bronze5/b2741.java
@@ -2,15 +2,15 @@ package bronze5;
 
 import java.util.Scanner;
 
-public class b2739 {
+public class b2741 {
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int A = scan.nextInt();
+		int N = scan.nextInt();
 		
-		for(int i=0;i<9;i++) {
-			System.out.println(A+" * "+i+" = "+(A*i));
+		for(int i=N-1;i>-1;i--) {
+			System.out.println(N-i);
 		}
 		
 		scan.close();
