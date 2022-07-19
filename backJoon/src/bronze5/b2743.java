8b29ba0466bea07397cf3b970ddb506f1320cba4
yeeun <dpdms9621@naver.com>
2022-07-19 18:10:59 +0900

    글자크기 출력

---
+++ backJoon/src/bronze5/b2743.java
@@ -0,0 +1,17 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class b2743 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String Word = scan.nextLine();
+		
+		System.out.println(Word.length());
+
+		scan.close();
+	}
+
+}
