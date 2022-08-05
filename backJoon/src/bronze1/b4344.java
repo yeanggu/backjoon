b8b1143b92050b82602e98f412f289b39ab86da6
yeeun <dpdms9621@naver.com>
2022-08-05 21:59:26 +0900

    평균은 넘겠지

---
+++ backJoon/src/bronze1/b4344.java
@@ -0,0 +1,45 @@
+package bronze1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class b4344 {
+//	OXÄûÁî
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int a = Integer.parseInt(br.readLine());
+		
+		
+		for(int i=0;i<a;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int b = Integer.parseInt(st.nextToken());
+			int[] c = new int[b];
+			int avg = 0;
+			int count = 0;
+			double per = 0;
+			
+			for(int j=0;j<b;j++) {
+				c[j] = Integer.parseInt(st.nextToken());
+				avg += c[j];
+			}
+			avg /= b;
+			for(int j=0;j<b;j++) {
+				if(c[j] > avg) {
+					count++;
+				}
+			}
+			per = Math.round((count/(double)b*100)*1000)/1000.0;
+			bw.write(String.format("%.3f", per)+"%\n");
+		}
+		
+		
+		bw.close();
+		br.close();
+	}
+	
+}
