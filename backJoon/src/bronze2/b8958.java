a4ded2f9ccf0cbf29c2f5b2513a998e011047318
yeeun <dpdms9621@naver.com>
2022-08-06 00:46:47 +0900

    OX퀴즈 추가 답안

--- backJoon/src/bronze2/b8958.java
+++ backJoon/src/bronze2/b8958.java
@@ -1,11 +1,39 @@
 package bronze2;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class b8958 {
 //	OXÄûÁî
-	
+	public static void main(String[] args) throws IOException {
+	      
+	      BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	      StringBuilder sb = new StringBuilder();
+	      
+	      int t = Integer.parseInt(bf.readLine());
+	      String str;
+	      int score = 0;
+	      int total = 0;
+	   
+	      for(int i = 0; i < t; i++) {
+	         str = bf.readLine();
+	         for(int j = 0; j < str.length(); j++) {
+	            score++;
+	            if(str.charAt(j) == 'X') {
+	               score = 0;
+	            }
+	            total += score;            
+	         }
+	         sb.append(total+"\n");
+	         score = 0;
+	         total = 0;
+	      }
+	      System.out.println(sb);
+	      bf.close();            
+	   }
+
+/*	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a = Integer.parseInt(br.readLine());
@@ -41,5 +69,5 @@ public class b8958 {
 		}
 		br.close();
 	}
-	
+*/	
 }
