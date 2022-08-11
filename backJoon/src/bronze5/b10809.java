b8fcdaa083f7db0ccbc2e308cccb95b4991821d8
yeeun <dpdms9621@naver.com>
2022-08-11 17:12:26 +0900

    알파벳 찾기

---
+++ backJoon/src/bronze5/b10809.java
@@ -0,0 +1,36 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class b10809 {
+//	알파벳 찾기
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[26];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = -1;
+		}
+ 
+		String S = br.readLine();
+ 
+		for(int i = 0; i < S.length(); i++) {
+			char ch = S.charAt(i);
+    
+			if(arr[ch - 'a'] == -1) {	// arr 원소 값이 -1 인 경우에만 초기화
+				arr[ch - 'a'] = i;
+			}
+		}
+ 
+		for(int val : arr) {	
+			System.out.print(val + " ");
+		}
+	}
+	
+}
