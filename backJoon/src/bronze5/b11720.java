c315252cca36044e9f62c129f60e59fb80fb1ace
yeeun <dpdms9621@naver.com>
2022-08-14 17:30:12 +0900

    shares

---
+++ backJoon/src/bronze5/b11720.java
@@ -0,0 +1,17 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class b11720 {
+	// Shares
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        int n, s;
+        while(sc.hasNext()){
+            n = sc.nextInt();
+            s = sc.nextInt();
+            System.out.println(s/(n+1));
+        }
+	}
+
+}
