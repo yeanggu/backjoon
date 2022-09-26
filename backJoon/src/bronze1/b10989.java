14283f33e294397ee47178436a67ac749bf4262b
yeeun <dpdms9621@naver.com>
2022-09-26 23:50:00 +0900

    수 정렬하기 3

---
+++ backJoon/src/bronze1/b10989.java
@@ -0,0 +1,32 @@
+package bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class b10989 {
+	//	수 정렬하기 3
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < N; i++) {
+			sb.append(i).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+	
+}
