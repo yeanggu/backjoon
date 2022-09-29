391a8bfc94c8b4abec9c762350e958b4b541f5a4
yeeun <dpdms9621@naver.com>
2022-09-29 21:18:51 +0900

    통계학

--- backJoon/src/silver3/b2108.java
+++ backJoon/src/silver3/b2108.java
@@ -4,30 +4,69 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
  
 public class b2108 {
-	// 커트라인
+	// 통계학
 	
 	public static void main(String[] args) throws IOException {
     
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-        
-		int N = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-
+		int N = Integer.parseInt(br.readLine());
+		
+		double avg = 0;
+		int median = 0;
+		int range = 0;
+		
 		Integer[] arr = new Integer[N];
         
-		st = new StringTokenizer(br.readLine());
-		
 		for(int i = 0; i < N; i++){
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(br.readLine());
+			avg += arr[i];
 		}
         
-		Arrays.sort(arr, Collections.reverseOrder());
-        
-		System.out.println(arr[k-1]);
+		Arrays.sort(arr);
+		
+		boolean flag = false;
+		int mode_max = 0;
+		int mode = 10000;
+		
+		for(int i = 0; i < N; i++) {
+			int jump = 0;	// 동일한 수가 나온만큼 i 값 jump 시킬 변수 
+			int count = 1;
+			int i_value = arr[i];
+			
+			for(int j = i + 1; j < N; j++){
+				if(i_value != arr[j]) {
+					break;
+				}
+				count++;
+				jump++;
+			}
+			
+			if(count > mode_max) {
+				mode_max = count;
+				mode = i_value;
+				flag = true;
+			}
+			else if(count == mode_max && flag == true) {
+				mode = i_value;
+				flag = false;
+			}
+			
+			i += jump;
+		}
+		
+		
+		
+		
+		avg /= N;
+		median = arr[N/2];
+		
+		range = arr[N-1]-arr[0];
+		
+		System.out.println(Math.round(avg));
+		System.out.println(median);
+		System.out.println(mode);
+		System.out.println(range);
 	}
 }
