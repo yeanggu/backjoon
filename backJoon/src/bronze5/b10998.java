4d7af2e66e11b88bebaee81e80cc961823c0d207
yeeun <dpdms9621@naver.com>
2022-10-06 23:29:51 +0900

    AXB

---
+++ backJoon/src/bronze5/b10998.java
@@ -0,0 +1,20 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b10998 {
+//	AXB
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a*b);
+	}
+	
+}
