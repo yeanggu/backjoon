e0489b2c3064a8734353d7d0ecb3386e4764977c
yeeun <dpdms9621@naver.com>
2022-10-16 17:46:31 +0900

    직각삼각형

--- backJoon/src/bronze1/b1259.java
+++ backJoon/src/bronze1/b1259.java
@@ -2,21 +2,19 @@ package bronze1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class b1259 {
 	//	ÆÓ¸°µå·Ò¼ö
 
 	public static void main(String[] args) throws Exception {
-		Scanner scan = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while(true){
-            int n = scan.nextInt();
-            if(n == 0){
+        	int N = Integer.parseInt(br.readLine());
+            if(N == 0){
                 break;
             }else{
                 int cnt = 0;
-                String str = Integer.toString(n);
+                String str = Integer.toString(N);
                 for(int i = 0; i < str.length()/2; i++){
                     if(str.charAt(i) == str.charAt(str.length()-1-i)){
                         cnt++;
@@ -29,7 +27,6 @@ public class b1259 {
                 }
             }
         }
-        scan.close();
 	}
 
 }
