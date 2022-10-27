fcff5b6e8375f98c94115312d6fff2174b9e0cc8
yeeun <dpdms9621@naver.com>
2022-10-27 17:02:40 +0900

    나누기

---
+++ backJoon/src/bronze2/b1075.java
@@ -0,0 +1,26 @@
+package bronze2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class b1075 {
+//	³ª´©±â
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int f = Integer.parseInt(br.readLine());
+		
+		int temp = (n/100)*100;
+		while(true) {
+			if(temp%f == 0) {
+				int result = temp % 100;
+				if(result < 10) System.out.println("0" + result);
+				else System.out.println(result);
+				return;
+			}
+			temp++;
+		}
+	}
+	
+}
