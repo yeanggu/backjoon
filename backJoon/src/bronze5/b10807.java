9a012aa4148230a18144b3e3a43479a0bcc0c1fe
yeeun <dpdms9621@naver.com>
2022-11-03 15:34:53 +0900

    개수 세기(hashmap)

---
+++ backJoon/src/bronze5/b10807.java
@@ -0,0 +1,34 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class b10807 {
+//	개수 세기
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < N; i++) {
+			int input = Integer.parseInt(st.nextToken());
+			if (map.get(input) == null) {
+				map.put(input, 1);
+			} else {
+				map.replace(input, map.get(input) + 1);
+			}
+		}
+
+		int V = Integer.parseInt(br.readLine());
+		if (map.get(V) == null)
+			System.out.println(0);
+		else
+			System.out.println(map.get(V));
+	}
+	
+}
