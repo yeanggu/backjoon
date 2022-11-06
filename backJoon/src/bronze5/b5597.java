97c085c25539d87b010df5d01f7f10d7b7b2b877
yeeun <dpdms9621@naver.com>
2022-11-06 20:56:16 +0900

    최댓값

--- backJoon/src/bronze5/b5597.java
+++ backJoon/src/bronze5/b5597.java
@@ -4,22 +4,34 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class b5597 {
-	//	과제 안 내신 분..?
-	
+	//	최댓값
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int[] student = new int[31];
-		
-		for(int i=1; i<29; i++) {
-			int success = Integer.parseInt(br.readLine());
-			student[success] = 1;
+		int[][] arr = new int[9][9];
+
+		for (int i = 0; i < 9; i++) {
+			String[] strArr = br.readLine().split(" ");
+			for (int j = 0; j < strArr.length; j++) {
+				arr[i][j] = Integer.parseInt(strArr[j]);
+			}
 		}
-		
-		for(int i=1; i<student.length; i++) {
-			if(student[i]!=1)
-				System.out.println(i);
+
+		int num = 0;
+		int row = 0;
+		int column = 0;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (num < arr[i][j]) {
+					num = arr[i][j];
+					row = i;
+					column = j;
+				}
+			}
 		}
+
+		System.out.println(num);
+		System.out.println((row + 1) + " " + (column + 1));
+
 	}
-	
-}
+}	
