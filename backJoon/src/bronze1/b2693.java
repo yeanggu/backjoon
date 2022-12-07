0d617e9e663cdd08ec3c4247f74d914605d273cc
yeeun <dpdms9621@naver.com>
2022-12-07 22:35:09 +0900

    N번째 큰 수

---
+++ backJoon/src/bronze1/b2693.java
@@ -0,0 +1,28 @@
+package bronze1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class b2693 {
+	//	N번째 큰 수
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < T; i++) {
+            int[] arr = new int[10];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 10; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            sb.append(arr[7] + "\n");
+        }
+        
+        System.out.print(sb);
+	}
+}
