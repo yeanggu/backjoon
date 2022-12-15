9d343cc6572dbfaa968186ab4f12e778922743f7
yeeun <dpdms9621@naver.com>
2022-12-15 23:17:54 +0900

    가장 큰 금민수

---
+++ backJoon/src/bronze1/b1526.java
@@ -0,0 +1,27 @@
+package bronze1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class b1526 {
+	//	가장 큰 금민수
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String N = br.readLine();
+
+		while(true) {
+			boolean check = true;
+			for(int i = 0; i < N.length(); i++) {
+				if(N.charAt(i) != '4' && N.charAt(i) != '7') {
+					check = false;
+					break;
+				}
+			}
+			if(check == true) break;
+			else N = String.valueOf(Integer.parseInt(N) - 1);
+		}
+		System.out.println(N);
+	}
+	
+}
