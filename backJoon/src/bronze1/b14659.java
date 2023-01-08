b03381208be6d8204907d823f42ac7e220b42400
yeeun <dpdms9621@naver.com>
2023-01-08 21:17:25 +0900

    한조서열정리하고옴 문제

---
+++ backJoon/src/bronze1/b14659.java
@@ -0,0 +1,40 @@
+package bronze1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+ 
+public class b14659 {
+	// 한조서열정리하고옴ㅋㅋ
+	
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N ;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int count = 0;
+		int result = Integer.MIN_VALUE;
+		
+		for(int i=0; i<N ; i++) {
+			count = 0;
+			for(int j=i+1; j<N ; j++) {
+				if(arr[i]>arr[j]) {
+					count++;
+				} else {
+					break;
+				}
+			}
+			result = Math.max(result, count);
+		}
+		
+		System.out.println(result);
+	}
+}
