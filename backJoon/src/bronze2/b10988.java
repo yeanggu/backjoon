00e4cdea145356162bace9f7775da730490425e5
yeeun <dpdms9621@naver.com>
2023-05-07 21:26:10 +0900

    팰린드롬인지 확인하기

---
+++ backJoon/src/bronze2/b10988.java
@@ -0,0 +1,33 @@
+package bronze2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class b10988 {
+	//	팰린드롬인지 확인하기
+	
+	public static void main(String[] args) throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        
+        int ans = 1;
+        
+        for (int i = input.length() - 1; i >= 0; i--) {
+            char c = input.charAt(i);
+ 
+            if (c != input.charAt(input.length() - i - 1)) {
+                ans = 0;
+                break;
+            }
+        }
+ 
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+	}        
+}
